9221a53c6ffeacedb522c71b8cfda0b649e51231
Usman-coder21 <usmanrafique078@gmail.com>
2025-03-20 04:20:49 +0500

    update

---
+++ src/app/menu/page.tsx
@@ -0,0 +1,29 @@
+import { menu } from "@/data";
+import Link from "next/link";
+import React from "react";
+
+const MenuPage = () => {
+  return (
+    <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-6rem)] lg:h-[calc(100vh)] flex flex-col md:flex-row items-center">
+      {menu.map((category) => (
+        <Link
+          href={`/menu/${category.slug}`}
+          key={category.id}
+          className="w-full h-1/3 bg-cover p-6 md:h-1/2 "
+          style={{ backgroundImage: `url(${category.img})` }}
+        >
+          <div className={`text-${category.color} w-1/2`}>
+            <h1 className="uppercase font-bold text-3xl">{category.title}</h1>
+            <p className="text-sm my-5">{category.desc}</p>
+            <button
+            style={{ backgroundColor: category.color + " !important" }} 
+            className={`custom-btn appearance-none hidden lg:block bg-${category.color} text-${category.color==="black" ? "white" : "red-500"} py-2 px-4 rounded-md `}>Explore
+            </button>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default MenuPage;
